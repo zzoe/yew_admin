24c4ee28c0d81c6419b5357d6f0fa73239abe186
zoe <zoe211@163.com>
2021-09-11 07:51:41 +0800

    no ybc, direct bulma

--- app/src/home.rs
+++ app/src/home.rs
@@ -1,10 +1,3 @@
-use ybc::{Column, ColumnProps, Columns, ColumnsProps};
-use ybc::{Container, ContainerProps};
-use ybc::Hero;
-use ybc::HeroProps;
-use ybc::HeroSize::*;
-use ybc::TileCtx::*;
-use ybc::TileSize::Three;
 use yew::prelude::*;
 
 pub struct Home {
@@ -24,7 +17,11 @@ impl Component for Home {
     type Properties = HomeProps;
 
     fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Self { props, link, label: "Menu label".to_owned() }
+        Self {
+            props,
+            link,
+            label: "Menu label".to_owned(),
+        }
     }
 
     fn update(&mut self, _msg: Self::Message) -> ShouldRender {
@@ -42,36 +39,60 @@ impl Component for Home {
 
     fn view(&self) -> Html {
         html! {
-            <ybc::Container fluid=true>
-                <ybc::Columns>
-                    <ybc::Column classes=classes!("is-narrow")>
-                        <ybc::Menu classes=classes!("menu")>
-                            <p class="menu-label">{ &*self.label }</p>
-                            <ul class="menu-list">
-                                <li><a>{"sldkfjdf"}</a></li>
-                                <li><a>{"sldkfjdf"}</a></li>
+            <div class="columns">
+              <div class="column is-narrow">
+                <div class="box">
+                    <aside class="menu">
+                      <p class="menu-label">
+                        {"General"}
+                      </p>
+                      <ul class="menu-list">
+                        <li><a>{"Dashboard"}</a></li>
+                        <li><a>{"Customers"}</a></li>
+                      </ul>
+                      <p class="menu-label">
+                        {"Administration"}
+                      </p>
+                      <ul class="menu-list">
+                        <li><a>{"Team Settings"}</a></li>
+                        <li>
+                          <a class="is-active">{"Manage Your Team"}</a>
+                          <ul>
+                            <li><a>{"Members"}</a></li>
+                            <li><a>{"Plugins"}</a></li>
+                            <li><a>{"Add a member"}</a></li>
+                          </ul>
+                        </li>
+                        <li><a>{"Invitations"}</a></li>
+                      </ul>
+                      <p class="menu-label">
+                        {"Transactions"}
+                      </p>
+                      <ul class="menu-list">
+                        <li><a>{"Payments"}</a></li>
+                        <li><a>{"Transfers"}</a></li>
+                        <li><a>{"Balance"}</a></li>
+                      </ul>
+                    </aside>
+                </div>
+              </div>
+              <div class="column">
+                <div class="box">
+                    // <nav class="panel is-fullwidth">
+                        <div class="tabs is-toggle is-fullwidth">
+                            <ul>
+                                <li class="is-active"><a>{"Pictures"}</a></li>
+                                <li><a>{"Music"}</a></li>
+                                <li><a>{"Videos"}</a></li>
+                                <li><a>{"Documents"}</a></li>
                             </ul>
-                        </ybc::Menu>
-                    </ybc::Column>
-                    <ybc::Column>
-                        <ybc::Hero fixed_nav=true size=FullheightWithNavbar body=html!{
-                            <p>{"Lorem ipsum dolor sit amet ..."}</p>
-                        } />
-                    </ybc::Column>
-                </ybc::Columns>
-                // <ybc::Tile ctx=Ancestor>
-                //   <ybc::Tile ctx=Parent vertical=true size=Three>
-                //     <ybc::Tile ctx=Child>
-                //
-                //     </ybc::Tile>
-                //   </ybc::Tile>
-                //   <ybc::Tile ctx=Parent vertical=true>
-                //     <ybc::Tile ctx=Child classes=classes!("box")>
-                //
-                //     </ybc::Tile>
-                //   </ybc::Tile>
-                // </ybc::Tile>
-            </ybc::Container>
+                        </div>
+                        // <div class="panel-block">
+                        // </div>
+                    // </nav>
+                </div>
+              </div>
+            </div>
         }
     }
 }
--- app/src/main.rs
+++ app/src/main.rs
@@ -1,4 +1,3 @@
-use ybc::NavbarFixed::Top;
 use yew::prelude::*;
 
 use home::Home;
@@ -37,68 +36,93 @@ impl Component for App {
         html! {
             <>
             <nav class="navbar" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <a class="navbar-item" href="https://bulma.io">
-      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
-    </a>
+              <div class="navbar-brand">
+                <a class="navbar-item" href="https://bulma.io">
+                  <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+                </a>
 
-    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
+                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                  <span aria-hidden="true"></span>
+                  <span aria-hidden="true"></span>
+                  <span aria-hidden="true"></span>
+                </a>
+              </div>
 
-  <div id="navbarBasicExample" class="navbar-menu">
-    <div class="navbar-start">
-      <a class="navbar-item">
-        {"Home"}
-      </a>
+              <div id="navbarBasicExample" class="navbar-menu">
+                <div class="navbar-start">
+                  <a class="navbar-item">
+                    {"Home"}
+                  </a>
 
-      <a class="navbar-item">
-        {"Documentation"}
-      </a>
+                  <a class="navbar-item">
+                    {"Documentation"}
+                  </a>
 
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link">
-          {"More"}
-        </a>
+                  <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link">
+                      {"More"}
+                    </a>
 
-        <div class="navbar-dropdown">
-          <a class="navbar-item">
-          {"About"}
-          </a>
-          <a class="navbar-item">
-          {"Jobs"}
-          </a>
-          <a class="navbar-item">
-          {"Contact"}
-          </a>
-          <hr class="navbar-divider" />
-          <a class="navbar-item">
-          {"Report an issue"}
-          </a>
-        </div>
-      </div>
-    </div>
+                    <div class="navbar-dropdown">
+                      <a class="navbar-item">
+                      {"About"}
+                      </a>
+                      <a class="navbar-item">
+                      {"Jobs"}
+                      </a>
+                      <a class="navbar-item">
+                      {"Contact"}
+                      </a>
+                      <hr class="navbar-divider" />
+                      <a class="navbar-item">
+                      {"Report an issue"}
+                      </a>
+                    </div>
+                  </div>
+                </div>
 
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <div class="buttons">
-          <a class="button is-primary">
-            <strong>{"Sign up"}</strong>
-          </a>
-          <a class="button is-light">
-          {"Log in"}
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
-</nav>
-            <ybc::Navbar fixed=Top />
+                <div class="navbar-end">
+                  <div class="navbar-item">
+                    <div class="buttons">
+                      <a class="button is-primary">
+                        <strong>{"Sign up"}</strong>
+                      </a>
+                      <a class="button is-light">
+                      {"Log in"}
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </nav>
             <Home />
-            <ybc::Footer />
+            <footer class="footer">
+              <div class="content has-text-centered">
+                <p>
+                  <strong>{"Bulma"}</strong>{" by "}<a href="https://jgthms.com">{"Jeremy Thomas"}</a>{". The source code is licensed"}
+                  <a href="http://opensource.org/licenses/mit-license.php">{"MIT"}</a>{". The website content
+                  is licensed "}<a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">{"CC BY NC SA 4.0"}</a>{"."}
+                </p>
+              </div>
+            </footer>
+
+            // <section class="hero is-fullheight">
+            //   // <!-- Hero head: will stick at the top -->
+            //   <div class="hero-head">
+            //
+            //   </div>
+            //
+            //   // <!-- Hero content: will be in the middle -->
+            //   <div class="hero-body p-0">
+            //     // <div class="container has-text-centered">
+            //     // </div>
+            //   </div>
+            //
+            //   // <!-- Hero footer: will stick at the bottom -->
+            //   <div class="hero-foot">
+            //
+            //   </div>
+            // </section>
             </>
         }
     }
